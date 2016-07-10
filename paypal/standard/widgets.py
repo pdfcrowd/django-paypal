71a630c85ef83869e4b1f865fb69a3af81c4287d
pdfcrowd dev <info+dev@pdfcrowd.com>
2016-07-10 09:29:48 +0200

    from django.forms.util -> .utils

--- paypal/standard/widgets.py
+++ paypal/standard/widgets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django import forms
-from django.forms.util import flatatt
+from django.forms.utils import flatatt
 from django.utils.safestring import mark_safe
 from django.utils.encoding import force_unicode
 
